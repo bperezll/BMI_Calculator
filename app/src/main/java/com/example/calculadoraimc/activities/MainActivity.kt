a52b946544cdf72c1a464a246f74b172c002bae0
bperezll <borja.perez.llano@gmail.com>
2024-02-11 21:06:07 +0100

    All functions working, bmiInformationText on development

--- app/src/main/java/com/example/calculadoraimc/activities/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/activities/MainActivity.kt
@@ -1,94 +1,103 @@
 package com.example.calculadoraimc.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.format.Formatter
-import android.util.Log
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatImageButton
 import androidx.appcompat.widget.AppCompatTextView
 import com.example.calculadoraimc.R
 import com.google.android.material.slider.Slider
+import java.math.RoundingMode
 import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var alturaResult:AppCompatTextView
-    lateinit var alturaSlider:Slider
+    // Declaring the needed variables before the onCreate
 
-    var altura:Float = 170F
+    lateinit var heightResult:AppCompatTextView
+    lateinit var heightSlider:Slider
 
-    lateinit var pesoResult:AppCompatTextView
-    lateinit var btnSumar:AppCompatButton
-    lateinit var btnRestar:AppCompatButton
+    var height:Float = 1.70F
 
-    var peso:Float = 60F
+    lateinit var weightResult:AppCompatTextView
+    lateinit var btnAdd:AppCompatImageButton
+    lateinit var btnSub:AppCompatImageButton
 
-    lateinit var btnCalcularIMC:AppCompatButton
-    lateinit var resultadoIMC:AppCompatTextView
+    var weight:Int = 60
 
-    var calculoFinal:Float = 0F
+    lateinit var btnCalculation:AppCompatButton
+    lateinit var finalResult:AppCompatTextView
+
+    var calculation:Float = 0F
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //alturaSeekBar.setOnClickListener() {
-            // Log.i("Testing", "El usuario ha introducido: ${userEditText.text}")
-        //}
+        // Call the IDs of the layout to be available to use
 
-        alturaResult = findViewById(R.id.alturaResult)
-        alturaSlider = findViewById(R.id.alturaSlider)
+        heightResult = findViewById(R.id.heightResult)
+        heightSlider = findViewById(R.id.heightSlider)
 
-        pesoResult = findViewById(R.id.pesoResult)
-        btnSumar = findViewById(R.id.btnSumar)
-        btnRestar = findViewById(R.id.btnRestar)
+        weightResult = findViewById(R.id.weightResult)
+        btnAdd = findViewById(R.id.btnAdd)
+        btnSub = findViewById(R.id.btnSub)
 
-        btnCalcularIMC = findViewById(R.id.btnCalcularIMC)
-        resultadoIMC = findViewById(R.id.resultadoIMC)
+        btnCalculation = findViewById(R.id.btnCalculation)
+        finalResult = findViewById(R.id.finalResult)
 
-        modificarAltura()
-        modificarPeso()
-        calcularIMC()
+        // Call the functions to execute them on the application
 
+        heightSelection()
+        weightSelection()
+        bmiCalculation()
     }
 
-    fun modificarAltura() {
+    private fun heightSelection() {
 
-        // Falta colocar formato para poner los decimales bien
+        // Default height on opening the application
 
-        alturaSlider.addOnChangeListener { _, value, _ ->
-            altura = value
-            alturaResult.text = value.toString()
-            //alturaSlider.valueFrom = 1.20F
-        }
+        heightResult.text = String.format(getString(R.string.height_result_text), height.toBigDecimal().setScale(2))
 
-    }
+        // Update height while move the slide
 
-    fun modificarPeso() {
-
-        pesoResult.text = peso.toString()
-
-        btnSumar.setOnClickListener() {
-            peso ++
-            pesoResult.text = peso.toString()
-        }
-
-        btnRestar.setOnClickListener() {
-            peso --
-            pesoResult.text = peso.toString()
-        }
-
-    }
-
-    fun calcularIMC() {
-
-        //resultadoIMC.text =
-
-        btnCalcularIMC.setOnClickListener() {
-            calculoFinal = peso / altura.pow(2).toString()
-            Log.i("IMC", "El IMC es ${resultadoIMC.text}")
+        heightSlider.addOnChangeListener { _, value, _ ->
+            height = value / 100
+            heightResult.text = String.format(getString(R.string.height_result_text), height.toBigDecimal().setScale(2))
         }
     }
 
+    private fun weightSelection() {
+
+        // Default weight on opening the application
+
+        weightResult.text = String.format(getString(R.string.weight_result_text), weight)
+
+        // Addition an Subtraction buttons to choose the weight
+
+        btnAdd.setOnClickListener() {
+            weight ++
+            weightResult.text = String.format(getString(R.string.weight_result_text), weight)
+        }
+
+        btnSub.setOnClickListener() {
+            weight --
+            weightResult.text = String.format(getString(R.string.weight_result_text), weight)
+        }
+    }
+
+    private fun bmiCalculation() {
+
+        // Final calculation to display result
+
+        btnCalculation.setOnClickListener() {
+            calculation = weight / height.pow(2)
+            val calculationRounded = calculation.toBigDecimal().setScale(1, RoundingMode.HALF_EVEN)
+            finalResult.text = calculationRounded.toFloat().toString()
+        }
+    }
+
+    private fun bmiInformationText() {
+
+    }
 }
